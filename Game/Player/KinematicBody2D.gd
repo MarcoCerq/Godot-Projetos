e2770600059004ef3550e6ac299587912c93e2ee
Stefano Dobbin <sndobbin@gmail.com>
2021-10-13 15:58:15 +0100

    Update
    Test LEvel Design Created
    Rough Player Movement Created

--- Game/Player/KinematicBody2D.gd
+++ Game/Player/KinematicBody2D.gd
@@ -1,17 +1,22 @@
 extends KinematicBody2D
 
-var motion = Vector2();
+const UP = Vector2(0, -1)
+var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += 10
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = 350
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -350
 	else:
 		motion.x = 0
 	
-	move_and_slide(motion)
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -300
+	
+	move_and_slide(motion, UP)
 	pass
-
+	
