3b8f63857c4cf0284d91ca2ae8a3cf965e789c44
MarcoCerq <marcocerqueira.official@gmail.com>
2021-10-13 16:41:18 +0100

    All

--- Game/Enemies/Melee/KinematicBody2D.gd
+++ Game/Enemies/Melee/KinematicBody2D.gd
@@ -1,15 +1,12 @@
 extends KinematicBody2D
 
-const Gravity = 10
-const Speed = 30
+const Gravity = 50
+const Speed = 200
 const Floor = Vector2(0, -1)
 
 var velocity = Vector2()
 
 var direction = 1
-
-func _ready():
-	pass # Replace with function body.
 
 
 func _physics_process(delta):
@@ -19,9 +16,9 @@ func _physics_process(delta):
 	
 	if is_on_wall():
 		direction = direction * -1
-		$RayCast2D. position.x *= -1
+		$RayCast2D.position.x *= -1
 		
-	if $RayCast2D. is_colliding() == false:
+	if $RayCast2D.is_colliding() == false:
 		direction = direction * -1
-		$RayCast2D. position.x *= -1
+		$RayCast2D.position.x *= -1
 		 
