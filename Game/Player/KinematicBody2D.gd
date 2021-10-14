b6c3ceed9a8370423b4bb051f10b487859076a95
Stefano Dobbin <sndobbin@gmail.com>
2021-10-14 11:54:31 +0100

    Changes
    Added Smooth movements to player
    Added Level Transition

--- Game/Player/KinematicBody2D.gd
+++ Game/Player/KinematicBody2D.gd
@@ -1,25 +1,51 @@
 extends KinematicBody2D
 
+# Setting up movement variables
 const UP = Vector2(0, -1)
 const GRAVITY = 50
-const SPEED = 300
+const ACCELERATION = 40
+const MAX_SPEED = 300
 const JUMP_HEIGHT = -700
 
 var motion = Vector2()
 
 func _physics_process(delta):
+	# Gravity
 	motion.y += GRAVITY
+	# Friction for acceleration/deseleration
+	var friction = false
 	
+	# If the player goes right, moves the character right and flips it's sprite right
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
+	# Else if the plaery goes left, moves the character left and flips it's sprite left
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
-		motion.x = 0
+	# Else makes sets it's movement to 0
+		$Sprite.play("Idle")
+		friction = true
+		motion.x = lerp(motion.x, 0, 0.30)
 	
+	# Checks if player is on floor, if he is and presses UP Arrow, he'll jump
+	# If player's not on floot, play fall animation
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.30)
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.1)
 	
+	# Sets player's current movent/jump speed
 	motion = move_and_slide(motion, UP)
 	pass
---
+++ Game/Player/PlayerDie.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+export(String, FILE, "*.tscn") var world_scene
+
+func _physics_process(delta):
+	# Create self-assigning var and set it to whatever it collides with
+	var bodies = $PlayerKinematicBody2D/Area2D.get_overlapping_bodies()
+	
+	# Loop collision for any body called "PlayerKinematicBody2D" it's bad ik
+	for body in bodies:
+		if body.name == "EnemyKinematicBody2D":
+			get_tree().change_scene(world_scene)
